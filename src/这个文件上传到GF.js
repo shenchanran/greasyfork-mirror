6a4b5c7ff8b771875b5dbe49c098330376d70564
shenchanran <14284537@qq.com>
2025-02-20 14:18:09 +0800

    first commit

---
+++ src/这个文件上传到GF.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         GreasyFork镜像站验证库
+// @namespace    申禅姌
+// @version      0.0.1
+// @description  ai题库对接
+// ==/UserScript==
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template:path.resolve(__dirname, './src/index.html')
+  })],
+};
