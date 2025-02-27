26bc0ebbbb8e5988f719e3c7a9baea042856910f
shenchanran <14284537@qq.com>
2025-02-27 21:48:08 +0800

    新增镜像站

--- src/这个文件上传到GF.js
+++ src/这个文件上传到GF.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         GreasyFork镜像站验证库-07
+// @name         GreasyFork镜像站验证库-08
 // @namespace    gfmirror
 // @version      0.0.1
 // @description  GreasyFork镜像站验证库:https://github.com/shenchanran/greasyfork-mirror
 // ==/UserScript==
 (function(){
-    let siteId = 6
+    let siteId = 7
     eval(`window.site${siteId}()`)
 })()
